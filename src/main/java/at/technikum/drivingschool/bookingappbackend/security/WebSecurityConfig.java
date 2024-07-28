f47516f16c02155d03708d7ad7d41d9732b03373
ZuzkaGlaess <zuzka.glaess@gmail.com>
2024-07-28 19:13:17 +0200

    Fixed Response when login or registration data validation fails Added messages for validation

--- src/main/java/at/technikum/drivingschool/bookingappbackend/security/WebSecurityConfig.java
+++ src/main/java/at/technikum/drivingschool/bookingappbackend/security/WebSecurityConfig.java
@@ -63,7 +63,8 @@ public class WebSecurityConfig {
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
+    http.cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable())
             // set handler for unauthorized access
         .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
             // set session policy

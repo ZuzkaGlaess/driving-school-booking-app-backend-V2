2c1bb13ab6999f8e2864564e7743d97198c8dbd2
ZuzkaGlaess <zuzka.glaess@gmail.com>
2024-10-08 17:55:14 +0200

    fixed tests as they broke with changed cookie behaviour

--- src/test/java/at/technikum/drivingschool/bookingappbackend/ProfilesTest.java
+++ src/test/java/at/technikum/drivingschool/bookingappbackend/ProfilesTest.java
@@ -1,6 +1,9 @@
 package at.technikum.drivingschool.bookingappbackend;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +16,6 @@ import java.net.URI;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc

df61ca7f28c376900950fa4de02bac2a44655961
ZuzkaGlaess <zuzka.glaess@gmail.com>
2024-10-20 21:51:49 +0200

    Sending validation error messages to consumer, added validation for password with pattern

--- src/main/java/at/technikum/drivingschool/bookingappbackend/dto/request/SignupRequest.java
+++ src/main/java/at/technikum/drivingschool/bookingappbackend/dto/request/SignupRequest.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
- 
+
 public class SignupRequest {
     @NotBlank(message = "Username not allowed to be empty")
     @Size(min = 5, max = 20, message = "Username has to be 5 till 20 characters")
     private String username;
- 
+
     @NotBlank(message = "Username not allowed to be empty")
     @Size(max = 50, message = "Email max. 50 characters")
     @Email(message = "Must be a well formated email address")
@@ -19,34 +19,35 @@ public class SignupRequest {
 
     @NotBlank(message = "Password is not allowed to be empty")
     @Size(min = 8, max = 30, message = "Password has to have min. 8 and max. 30 characters")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,30}$", message = "Password has to contain at least one uppercase, low case, number and special character: @#$%^&+=")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,30}$",
+            message = "Password has to contain at least one uppercase, low case, number and special character: @#$%^&+=")
     private String password;
 
     private EGender gender;
     private String other;
 
     private Country country;
-  
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
     }
- 
+
     public String getEmail() {
         return email;
     }
- 
+
     public void setEmail(String email) {
         this.email = email;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
